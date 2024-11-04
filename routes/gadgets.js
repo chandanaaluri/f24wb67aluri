cc60a02cf8aa1d6b73569f9f72ca5e40bc62b7a7
chandanaaluri <chandanaaluri05@gmail.com>
2024-11-04 11:51:27 -0600

    updated

--- routes/gadgets.js
+++ routes/gadgets.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 // Define the GET route for gadgets
-router.get('/gadgets', (req, res) => {
+router.get('/', (req, res) => {
+  // This should match the name of your Pug file in views
   res.render('gadgets', { title: 'Search Results - Gadgets' });
 });
 
