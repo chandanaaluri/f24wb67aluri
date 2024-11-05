c4736850af3b0ace180cefc5c886510830bea9e9
chandanaaluri <chandanaaluri05@gmail.com>
2024-11-05 11:09:48 -0600

    updated

--- app.js
+++ app.js
@@ -6,15 +6,12 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const gadgetsRouter = require('./routes/gadgets');
 const gridRouter = require('./routes/grid');
-const pickRouter = require('./routes/pick'); // Make sure this path is correct
-//app.use('/selector', pickRouter); // Make sure the route is set up as /selector
-
+const pickRouter = require('./routes/pick'); // Ensure this path is correct
 
 var app = express();
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -27,16 +24,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Set up routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/gadgets', gadgetsRouter);
 app.use('/grid', gridRouter);
 app.use('/selector', pickRouter); // Add the /selector route for random item display
 
-// catch 404 and forward to error handler
+// Add the /gadgets route
+app.get('/gadgets', (req, res) => {
+  const results = [
+    { gadget_name: 'Smartwatch', price: 199, functionality: 'Fitness Tracking' },
+    { gadget_name: 'VR Headset', price: 299, functionality: 'Virtual Reality Experience' },
+    { gadget_name: 'Drone', price: 499, functionality: 'Aerial Photography' }
+  ];
+  res.render('gadgets', { results }); // Pass results to Pug
+});
+
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
